8e49029f3e75ead47270bb60b9aa5a3eeb386aae
Aron Granberg <aron.granberg@gmail.com>
2020-07-31 13:23:13 +0200

    Add thumbnails

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ env = Environment(
 )
 
 
+@app.route('/static/thumbnails/<path:p>')
+def thumbnail(p):
+    path = get_thumbnail(p)[0]
+    if path is None:
+        abort(404)
+    return app.send_static_file(path)
+
+
 @app.route('/')
 def index():
     template = env.get_template('index.html')
@@ -27,31 +35,83 @@ def index():
 
 image_extensions = [".jpg", ".png", ".jpeg", ".gif"]
 
+thumbnails = dict()
+cached_image_info = dict()
+
+for OrientationKey in ExifTags.TAGS.keys():
+    if ExifTags.TAGS[OrientationKey] == 'Orientation':
+        break
+
+
+def adjust_rotation(image, exif):
+    if OrientationKey in exif:
+        if exif[OrientationKey] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[OrientationKey] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[OrientationKey] == 8:
+            image = image.rotate(90, expand=True)
+    return image
+
+
+def get_thumbnail(path):
+    assert ".." not in path
+    if path not in thumbnails:
+        print("Generating thumbnail for " + path)
+        try:
+            image = Image.open("static/" + path)
+        except Exception as e:
+            print("Could not open image")
+            print(e)
+            return None
+
+        exif = dict(image.getexif().items())
+        image.thumbnail(size=(1000, 300))
+        image = adjust_rotation(image, exif)
+
+        result_path = "thumbnails/" + path
+        os.makedirs("static/" + os.path.dirname(result_path), exist_ok=True)
+        image.save("static/" + result_path, quality=90)
+        thumbnails[path] = (result_path, image.size)
+
+    return thumbnails[path]
+
 
 def find_images():
     images = []
 
-    for orientation in ExifTags.TAGS.keys():
-        if ExifTags.TAGS[orientation] == 'Orientation':
-            break
-    
     for folder in image_folders:
         for file in os.listdir("static/" + folder):
             if not any(file.endswith(ext) for ext in image_extensions):
                 continue
 
             path = folder + "/" + file
-            image = Image.open("static/" + path)
+            if path in cached_image_info:
+                images.append(cached_image_info[path])
+            else:
+                image = Image.open("static/" + path)
 
-            exif = dict(image.getexif().items())
+                # exif = dict(image.getexif().items())
 
-            size = image.size
+                # size = image.size
 
-            if orientation in exif and (exif[orientation] in [6, 8]):
-                size = (size[1], size[0])
+                accurate = False
+                if path in thumbnails:
+                    (_, thumbnail_size) = get_thumbnail(path)
+                    accurate = True
+                else:
+                    exif = dict(image.getexif().items())
+                    thumbnail_size = image.size
+                    if OrientationKey in exif and (exif[OrientationKey] in [6, 8]):
+                        thumbnail_size = (thumbnail_size[1], thumbnail_size[0])
 
-            date = datetime.strptime(image.getexif()[36867], r"%Y:%m:%d %H:%M:%S")
-            images.append((path, size, date))
+                date = datetime.strptime(image.getexif()[36867], r"%Y:%m:%d %H:%M:%S")
+                item = (path, thumbnail_size, date)
+
+                if accurate:
+                    cached_image_info[path] = item
+
+                images.append(item)
 
     images.sort(key=lambda x: x[2], reverse=True)
     res = groupby(images, key=lambda x: datetime(year=x[2].year, month=x[2].month, day=x[2].day))
